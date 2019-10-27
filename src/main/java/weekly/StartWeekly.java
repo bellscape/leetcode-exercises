dfed1ba22181c89be81f03cd6751001b1f154f4f
bell <bellscape@gmail.com>
2019-10-27 08:45:46 +0800

    + weekly starter

---
+++ src/main/java/weekly/StartWeekly.java
@@ -0,0 +1,36 @@
+package weekly;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+import static java.lang.String.format;
+
+public class StartWeekly {
+
+  public static void start(int c) throws Exception {
+    // create package dir
+    String path = format("src/main/java/weekly/c%s", c);
+    new File(path).mkdirs();
+    for (int p = 1; p <= 4; p++) {
+      // create class file
+      try (PrintWriter clazz = new PrintWriter(format("%s/c%sp%s.java", path, c, p), StandardCharsets.UTF_8.name())) {
+        clazz.println(format("package weekly.c%s;\n", c));
+        clazz.println("import common.Util;\n");
+        clazz.println(format("public class c%sp%s {\n\n", c, p));
+        clazz.println("  public static void main(String[] args) { Util.runFiles(); }\n\n}\n");
+      }
+
+      // create example file
+      try (FileOutputStream example = new FileOutputStream(format("%s/c%sp%s-example.txt", path, c, p))) {
+        // empty file
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    start(Integer.parseInt(args[0]));
+  }
+
+}
