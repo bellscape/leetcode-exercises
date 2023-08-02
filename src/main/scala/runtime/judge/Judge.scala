f71ccc2ad14db65756a07779753987a8d9a2f985
bell <bell>
2023-08-02 19:58:41 +0100

    scala: judge

---
+++ src/main/scala/runtime/judge/Judge.scala
@@ -0,0 +1,51 @@
+package runtime.judge
+
+import runtime.parser.{Example, ObjectReader}
+
+import java.lang.reflect.{Method, Modifier}
+
+case class JudgeResult(cost: Long, wrong_answer: Boolean, expect_output: String, actual_output: String) {
+	def time_limit_exceeded: Boolean = cost > 5_000
+	def ok: Boolean = !wrong_answer && !time_limit_exceeded
+}
+
+object Judge {
+
+	def run(method: Method, examples: Seq[Example]): Seq[JudgeResult] = {
+		assert(!Modifier.isStatic(method.getModifiers))
+		assert(Modifier.isPublic(method.getModifiers))
+
+		examples.map(example => {
+			val start = System.currentTimeMillis()
+			val returned = call(method, example.input)
+			val cost = System.currentTimeMillis() - start
+			compare_result(method, example.output, returned, cost)
+		})
+	}
+
+	private def call(method: Method, input: String): Any = {
+		val reader = new ObjectReader(input)
+		val args: Array[Any] = method.getParameterTypes.map(reader.read_param)
+		assert(reader.eof)
+
+		val instance = method.getDeclaringClass.getDeclaredConstructor().newInstance()
+		method.invoke(instance, args: _*)
+	}
+
+	private def compare_result(method: Method, expect: String, actual: Any, cost: Long): JudgeResult = {
+		val expect_obj = new ObjectReader(expect).read_return(method.getReturnType)
+		val wrong_answer = expect_obj != actual
+		if (wrong_answer) {
+			JudgeResult(cost, wrong_answer, expect, format_obj(actual))
+		} else {
+			JudgeResult(cost, wrong_answer, expect, expect)
+		}
+	}
+	private def format_obj(obj: Any): String = {
+		obj match {
+			case arr: Array[_] => arr.map(format_obj).mkString("[", ",", "]")
+			case _ => obj.toString
+		}
+	}
+
+}
---
+++ src/test/scala/runtime/judge/TestJudge.scala
@@ -0,0 +1,21 @@
+package runtime.judge
+
+import org.scalatest.funsuite.AnyFunSuite
+import runtime.parser.Example
+
+class TestJudge extends AnyFunSuite {
+
+	def sample_add(a: Int, b: Int): Int = a + b
+
+	test("sample") {
+		val method = classOf[TestJudge].getDeclaredMethod("sample_add", classOf[Int], classOf[Int])
+		val example_1 = Example("1,2", "3") // ok
+		val example_2 = Example("1,2", "4") // wrong answer
+
+		val results = Judge.run(method, Seq(example_1, example_2))
+		assert(results.size == 2)
+		assert(results(0).ok)
+		assert(!results(1).ok)
+	}
+
+}
