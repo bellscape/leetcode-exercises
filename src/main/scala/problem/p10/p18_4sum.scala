318dbfb3dab02437ebb80906871c5aa9425ac703
bell <bell>
2023-08-10 19:12:47 +0100

    p18

--- src/main/scala/problem/p10/p18_4sum.scala
+++ src/main/scala/problem/p10/p18_4sum.scala
@@ -7,54 +7,56 @@ import scala.collection.mutable.ArrayBuffer
 
 object p18_4sum extends WithMain {
 	def fourSum(nums: Array[Int], target: Int): List[List[Int]] = {
-		val n = nums.length
 		util.Arrays.sort(nums)
 		val out = ArrayBuffer.empty[List[Int]]
 
-		var i1 = 0
-		while (i1 <= n - 4) {
-			val v1 = nums(i1)
+		def iterate(from: Int, to: Int, f: (Int, Int) => Unit): Unit = {
+			var i = from
+			while (i <= to) {
+				val v = nums(i)
+				f(i, v)
 
-			var i2 = i1 + 1
-			while (i2 <= n - 3) {
-				val v2 = nums(i2)
+				while (i + 1 <= to && nums(i + 1) == v)
+					i += 1
+				i += 1
+			}
+		}
+
+		def twin_iterate(left_from: Int, right_from: Int, f: (Int, Int) => (Boolean, Boolean)): Unit = {
+			var left = left_from
+			var right = right_from
+			while (left < right) {
+				val v_left = nums(left)
+				val v_right = nums(right)
+				val (move_left, move_right) = f(v_left, v_right)
+
+				if (move_left) {
+					while (left + 1 < right && nums(left + 1) == v_left)
+						left += 1
+					left += 1
+				}
+				if (move_right) {
+					while (left < right - 1 && nums(right - 1) == v_right)
+						right -= 1
+					right -= 1
+				}
+			}
+		}
+
+		val n = nums.length
+		iterate(0, n - 4, (i1, v1) => {
+			iterate(i1 + 1, n - 3, (i2, v2) => {
 				val target_34 = target.toLong - v1 - v2
-
-				var i3 = i2 + 1
-				var i4 = n - 1
-				while (i3 < i4) {
-					val v3 = nums(i3)
-					val v4 = nums(i4)
+				twin_iterate(i2 + 1, n - 1, (v3, v4) => {
 					val sum_34 = v3.toLong + v4.toLong
 					if (sum_34 == target_34) {
 						out += List(v1, v2, v3, v4)
 					}
 
-					if (sum_34 <= target_34) {
-						// next i3
-						while (i3 + 1 < i4 && nums(i3 + 1) == v3)
-							i3 += 1
-						i3 += 1
-					}
-					if (sum_34 >= target_34) {
-						// next i4
-						while (i3 < i4 - 1 && nums(i4 - 1) == v4)
-							i4 -= 1
-						i4 -= 1
-					}
-				}
-
-				// next i2
-				while (i2 + 1 < n && nums(i2 + 1) == v2)
-					i2 += 1
-				i2 += 1
-			}
-
-			// next i1
-			while (i1 + 1 < n && nums(i1 + 1) == v1)
-				i1 += 1
-			i1 += 1
-		}
+					(sum_34 <= target_34, sum_34 >= target_34)
+				})
+			})
+		})
 
 		out.toList
 	}
