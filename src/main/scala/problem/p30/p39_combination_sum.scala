7d414cb2191d66ea71149ad7ca37da3d531dcc5f
bell <bell>
2023-09-12 23:31:11 +0100

    p39

---
+++ src/main/scala/problem/p30/p39_combination_sum.scala
@@ -0,0 +1,48 @@
+package problem.p30
+
+import runtime.WithMain
+
+import java.util
+import scala.collection.mutable.ListBuffer
+
+object p39_combination_sum extends WithMain {
+	def combinationSum(candidates: Array[Int], target: Int): List[List[Int]] = {
+		assert(candidates.nonEmpty)
+		assert(target > 0)
+
+		util.Arrays.sort(candidates)
+		assert(candidates.head >= 2)
+
+		val out = ListBuffer.empty[List[Int]]
+
+		def dfs(i: Int, left: Int, prev: List[Int]): Unit = {
+			// case: 可用组合
+			if (left == 0) {
+				out += prev.reverse
+				return
+			}
+
+			// case: 优化：最后一个数字
+			val num = candidates(i)
+			if (i == candidates.length - 1) {
+				if (left % num == 0) {
+					out += prev.reverse ++ List.fill(left / candidates(i))(candidates(i))
+				}
+				return
+			}
+
+			// case: 无合适数字
+			if (left < num) return
+
+			// try: 选当前数字
+			dfs(i, left - num, num :: prev)
+
+			// try: 不选当前数字
+			dfs(i + 1, left, prev)
+		}
+
+		dfs(0, target, List.empty[Int])
+
+		out.toList
+	}
+}
