d4a62f7cfc3680de333cde34eb592096b05fc201
bell <bell>
2023-09-15 18:51:01 +0100

    p42

--- src/main/scala/problem/p40/p42_trapping_rain_water.scala
+++ src/main/scala/problem/p40/p42_trapping_rain_water.scala
@@ -4,30 +4,37 @@ import runtime.WithMain
 
 object p42_trapping_rain_water extends WithMain {
 	def trap(height: Array[Int]): Int = {
+		assert(height.nonEmpty)
 
-		// step: 假定右侧无限高，调整水位
-		val water = new Array[Int](height.length)
-		var cur = 0
-		for (i <- height.indices) {
-			val h = height(i)
-			if (h > cur) cur = h
+		// 思路：由两侧向中间，逐渐确定每个位置的水位
 
-			water(i) = cur
+		var sum = 0
+
+		var left_i = 0
+		var left_h = height(left_i)
+		var right_i = height.length - 1
+		var right_h = height(right_i)
+
+		while (left_i < right_i) {
+			if (left_h < right_h) {
+				// 左侧低，移动左侧
+				sum += left_h - height(left_i)
+
+				left_i += 1
+				val h = height(left_i)
+				if (h > left_h) left_h = h
+			} else {
+				// 右侧低，移动右侧
+				sum += right_h - height(right_i)
+
+				right_i -= 1
+				val h = height(right_i)
+				if (h > right_h) right_h = h
+			}
 		}
 
-		// step: 右侧恢复正常，调整水位
-		cur = 0
-		for (i <- height.indices.reverse) {
-			val h = height(i)
-			if (h > cur) cur = h
+		sum += left_h.min(right_h) - height(left_i)
 
-			val w = water(i)
-			if (w > cur) water(i) = cur
-		}
-
-		// step: output
-		height.indices
-			.map(i => water(i) - height(i))
-			.sum
+		sum
 	}
 }
